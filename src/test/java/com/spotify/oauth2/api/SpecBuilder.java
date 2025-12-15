4e1ac2a4a9bcd2f99192d2b07482e86899e09007
Akshay <akshaynikhare@gmail.com>
2025-12-15 12:55:51 +0530

    First Change

---
+++ src/test/java/com/spotify/oauth2/api/SpecBuilder.java
@@ -0,0 +1,44 @@
+package com.spotify.oauth2.api;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static com.spotify.oauth2.api.Route.BASE_PATH;
+import static com.spotify.oauth2.api.Route.BASE_URI;
+//import static com.spotify.oauth2.api.Route.ACCOUNT_BASE_URI;
+
+
+public class SpecBuilder {
+
+    public static RequestSpecification getRequestSpec(){
+        return new RequestSpecBuilder().
+               // setBaseUri(BASE_URI).
+               setBaseUri("https://api.spotify.com").
+                setBasePath(BASE_PATH).
+                setContentType(ContentType.JSON).
+                addFilter(new AllureRestAssured()).
+                log(LogDetail.ALL).
+                build();
+    }
+
+    public static RequestSpecification getAccountRequestSpec(){
+        return new RequestSpecBuilder().
+                // setBaseUri(System.getProperty("ACCOUNT_BASE_URI")).
+               setBaseUri("https://accounts.spotify.com").
+                setContentType(ContentType.URLENC).
+                addFilter(new AllureRestAssured()).
+                log(LogDetail.ALL).
+                build();
+    }
+
+    public static ResponseSpecification getResponseSpec(){
+        return new ResponseSpecBuilder().
+                log(LogDetail.ALL).
+                build();
+    }
+}
